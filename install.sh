2e37152e888004e4fbe5e5c12bd40108c398feff
BKAPAY <kejatransfer1@gmail.com>
2026-06-25 21:42:32 +0100

    Update install.sh to clear cache and force proper registry

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export NPM_CONFIG_REGISTRY=https://registry.npmjs.org/
-export NPM_CONFIG_STRICT_SSL=false
-npm install --registry https://registry.npmjs.org/
+npm cache clean --force
+npm config set registry https://registry.npmjs.org/
+npm install
