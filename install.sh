df68b344e33b50673d11e68ff8efec2ca0116196
BKAPAY <kejatransfer1@gmail.com>
2026-06-25 21:58:03 +0100

    Update install script with legacy-peer-deps flag

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm cache clean --force
-npm config set registry https://registry.npmjs.org/
-npm install
+npm config set registry https://registry.npmjs.org/ --global
+npm install --legacy-peer-deps
